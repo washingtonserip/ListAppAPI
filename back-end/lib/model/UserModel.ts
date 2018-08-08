17e60d5dee7a6e312f9c4ef9952f59eb89db1a23
Washington Pires <4washingtonbr@gmail.com>
2018-08-08 01:13:05 -0300

    [back-end] User model implementation.

---
+++ back-end/lib/model/UserModel.ts
@@ -0,0 +1,75 @@
+import * as mongoose from 'mongoose';
+import { UserSchema } from '../schemas/UserSchema';
+import { IUserModel } from '../interfaces/userInterface';
+import { Promise } from 'es6-promise';
+
+export class UserModel {
+  private userSchema: mongoose.Model<IUserModel> = UserSchema;
+
+  public create (name: string, email: string, username: string, password: string) {
+    const user = <IUserModel>{
+      name,
+      email,
+      username,
+      password
+    };
+
+    return new Promise((resolve, reject) => {
+      this.userSchema.create(user, (error, user) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(user);
+        }
+      })
+    });
+  }
+
+  public find () {
+    return new Promise((resolve, reject) => {
+      this.userSchema.find({}, (error, users) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(users);
+        }
+      })
+    });
+  }
+
+  public findById (id) {
+    return new Promise((resolve, reject) => {
+      this.userSchema.findById(id, (error, user) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(user);
+        }
+      })
+    });
+  }
+
+  public update (id, body) {
+    return new Promise((resolve, reject) => {
+      this.userSchema.findByIdAndUpdate(id, body, { new: true }, (error, user) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(user);
+        }
+      })
+    });
+  }
+
+  public delete (id) {
+    return new Promise((resolve, reject) => {
+      this.userSchema.findByIdAndRemove(id, (error, user) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(user);
+        }
+      })
+    });
+  }
+}
