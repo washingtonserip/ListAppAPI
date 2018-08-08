6dd41b894e616bb6102378d555a4756ff2ea63dc
Washington Pires <4washingtonbr@gmail.com>
2018-08-08 01:21:40 -0300

    [back-end] Database connect fix.

--- back-end/lib/DataBase.ts
+++ back-end/lib/DataBase.ts
@@ -2,6 +2,6 @@ import * as mongoose from 'mongoose';
 
 export default class DataBase {
   constructor () {
-    mongoose.connect('mongodb://test:test123@ds018308.mlab.com:18308/node-test');
+    mongoose.connect('mongodb://' + process.env.MONGODB_URI);
   }
 }
