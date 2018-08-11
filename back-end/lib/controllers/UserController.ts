539b23731c2f3fe14afe56f4bde661eaf70cf80d
Washington Pires <4washingtonbr@gmail.com>
2018-08-11 00:47:08 -0300

    [back-end] Login API implementation.

--- back-end/lib/controllers/UserController.ts
+++ back-end/lib/controllers/UserController.ts
@@ -1,8 +1,37 @@
 import * as express from 'express';
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import Config from '../Config';
 import { UserRepository } from '../repositories/UserRepository';
 import { IUser } from '../interfaces/UserInterface';
 
 export class UserController {
+  public login (request: express.Request, response: express.Response) {
+    const _userRepository = new UserRepository();
+    const { email, password } = request.body;
+
+    _userRepository.findOne({ email })
+      .then((result) => {
+        if (!result) {
+          return response.status(404).send('No user found.');
+        }
+
+        const passwordIsValid = password ? bcrypt.compareSync(password, result.password) : false;
+        if (!passwordIsValid) {
+          return response.status(401).send({ auth: false, token: null });
+        }
+
+        const token = jwt.sign({ id: result._id }, Config.secret, {
+          expiresIn: 86400
+        });
+
+        response.status(200).send({ auth: true, token });
+      })
+      .catch((error) => {
+        response.status(500).send(error);
+      });
+  }
+
   public create (request: express.Request, response: express.Response) {
     const { name, email, username, password } = request.body;
     const payload: IUser = { name, email, username, password };
