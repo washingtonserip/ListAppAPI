679fd9e0e45345f2350f897217f002fbffe62fe5
Washington Pires <4washingtonbr@gmail.com>
2018-08-11 03:52:27 -0300

    [back-end] User interface fix.

--- back-end/lib/interfaces/UserInterface.ts
+++ back-end/lib/interfaces/UserInterface.ts
@@ -1,22 +1,22 @@
 import * as mongoose from 'mongoose';
 
 export interface IUser {
-  name: string,
-  email: string,
-  username: string,
-  password: string,
-  avatar?: string,
-  createdAt?: Date,
-  modifiedAt?: Date
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+  avatar?: string;
+  createdAt?: Date;
+  modifiedAt?: Date;
 }
 
 export interface IUserModel extends IUser, mongoose.Document {}
 
 export interface IUserRepository {
-  create(IUser): Promise<IUserModel>;
+  create(payload: IUser): Promise<IUserModel>;
   find(params?: object): Promise<IUserModel[]>;
   findById(id: string): Promise<IUserModel>;
   findOne(params: object): Promise<IUserModel>;
-  update(string, IUser): Promise<IUserModel>;
+  update(id: string, body: IUser): Promise<IUserModel>;
   delete(id: string): Promise<IUserModel>;
 }
