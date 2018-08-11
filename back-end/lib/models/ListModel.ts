215fbf7083a9c50c0475f4a13555e386b1215cf5
Washington Pires <4washingtonbr@gmail.com>
2018-08-11 03:42:18 -0300

    [back-end] List model  implementation.

---
+++ back-end/lib/models/ListModel.ts
@@ -0,0 +1,38 @@
+import * as mongoose from 'mongoose';
+import { IListModel } from '../interfaces/ListInterface';
+
+const CheckListSchema = new mongoose.Schema({
+  text: String,
+  isCheck: Boolean
+});
+CheckListSchema.add({ list: [CheckListSchema] });
+
+const schema: mongoose.Schema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: false
+  },
+  list: {
+    type: [CheckListSchema],
+    required: false
+  },
+  createdAt: {
+    type: Date,
+    required: false
+  },
+  modifiedAt: {
+    type: Date,
+    required: false
+  }
+});
+
+mongoose.model('List', schema);
+export const ListSchema = mongoose.model<IListModel>('List');
