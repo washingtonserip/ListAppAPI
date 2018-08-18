f739e7b611b26da991967704d6554a0bc885e2c3
Washington Pires <4washingtonbr@gmail.com>
2018-08-18 19:24:41 -0300

    [back-end] Environment refactoring.

--- back-end/lib/Server.ts
+++ back-end/lib/Server.ts
@@ -1,4 +1,6 @@
+import './DevelopmentEnviroment';
 import app from './App';
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Express server listening on port ' + port));
