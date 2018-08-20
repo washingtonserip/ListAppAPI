0caef2b8c2b41f9dc17dacc39ed6a6572e271fe8
Washington Pires <4washingtonbr@gmail.com>
2018-08-20 00:45:47 -0300

    [front-end] Updated Footer component.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './configs/routes';
+import { ComponentsModule } from './components/components.module';
 
 import { HomeModule } from './pages/home/home.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ComponentsModule,
     HomeModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
--- front-end/src/app/pages/home/home.module.ts
+++ front-end/src/app/pages/home/home.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FontAwesomeModule,
-    ComponentsModule
+    FontAwesomeModule
   ],
   declarations: [HomeComponent],
   exports: [HomeComponent]
