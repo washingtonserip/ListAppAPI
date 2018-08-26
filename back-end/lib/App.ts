f168f0c4014606b051a3f87149b98f74bfa521ee
Washington Pires <4washingtonbr@gmail.com>
2018-08-26 16:48:52 -0300

    [back-end] Added Cors.

--- back-end/lib/App.ts
+++ back-end/lib/App.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import Database from './Database';
 import { UserRoutes } from './routes/UserRoutes';
 import { ListRoutes } from './routes/ListRoutes';
@@ -9,6 +10,7 @@ class App {
 
   constructor() {
     this.app = express();
+    this.app.use(cors());
 
     this.app.use('/user', new UserRoutes().routes);
     this.app.use('/list', new ListRoutes().routes);
