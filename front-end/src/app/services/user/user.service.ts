1a8150e1439d482acbf87a407d10e45d1cf9adaa
Washington Pires <4washingtonbr@gmail.com>
2018-08-26 19:18:31 -0300

    [front-end] Moving User service.

--- front-end/src/app/services/user/user.service.ts
+++ front-end/src/app/services/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IUser } from '../interfaces/user.interface';
-import { environment } from '../../environments/environment';
+import { IUser } from '../../interfaces/user.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
