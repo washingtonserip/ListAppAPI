ede627ad9d271f95a9e3591cb2eb8e7cc901e2b3
Washington Pires <4washingtonbr@gmail.com>
2018-08-26 19:02:38 -0300

    [front-end] Integration with the service to register user.

--- front-end/src/app/pages/auth/auth.component.ts
+++ front-end/src/app/pages/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { faHome, faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 import { Menu } from '../../interfaces/menu.interface';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'component-auth',
@@ -17,10 +18,68 @@ export class AuthComponent {
       isInverted: true
     }
   ];
-  faSignInAlt = faSignInAlt;
-  faUserPlus = faUserPlus;
+  public error;
+  public success;
+  public emailError;
+  public passwordError;
+  public email = '';
+  public password = '';
+  public faSignInAlt = faSignInAlt;
+  public faUserPlus = faUserPlus;
 
   constructor (
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) {}
+
+  validateEmail (email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  validateForm (callback) {
+    const user = {
+      email: this.email,
+      password: this.password,
+    };
+    let errorCount = 0;
+
+    this.emailError = '';
+    this.passwordError = '';
+
+    if (!this.email) {
+      this.emailError = 'Can\'t be blank';
+      errorCount++;
+    } else if (!this.validateEmail(this.email)) {
+      this.emailError = 'Email is invalid';
+      this.email = '';
+      errorCount++;
+    }
+
+    if (!this.password) {
+      this.passwordError = 'Can\'t be blank';
+      errorCount++;
+    } else if (this.password.length < 6) {
+      this.passwordError = 'Password is too short (minimum is 6 characters)';
+      this.password = '';
+      errorCount++;
+    }
+
+    this.success = '';
+    this.error = '';
+
+    if (errorCount === 0) {
+      this.email = '';
+      this.password = '';
+      return callback(user);
+    }
+  }
+
+  createAccount (user) {
+    this.userService.createUser(user)
+      .subscribe(
+        response => { this.success = 'Account created successfully. Please login.'; },
+        error => { this.error = 'An error has occurred. Try again.'; }
+      );
+  }
 }
--- front-end/src/app/pages/auth/auth.module.ts
+++ front-end/src/app/pages/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { AuthComponent } from './auth.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ComponentsModule } from '../../components/components.module';
+import { AuthComponent } from './auth.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     ComponentsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    HttpClientModule
   ],
   declarations: [AuthComponent],
   exports: [AuthComponent]
