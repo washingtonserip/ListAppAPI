556b49e07e627e717f21a9bcedecc211b8d61eab
Washington Pires <4washingtonbr@gmail.com>
2018-09-01 17:43:14 -0300

    [front-end] Added CRUD methods in List service.

--- front-end/src/app/services/list/list.service.ts
+++ front-end/src/app/services/list/list.service.ts
@@ -22,4 +22,19 @@ export class ListService {
     const urlRequest = `${this.serverUrl}/list`;
     return this.httpClient.post<IList>(urlRequest, theList);
   }
+
+  read (listId: string) {
+    const urlRequest = `${this.serverUrl}/list/${listId}`;
+    return this.httpClient.get<IList>(urlRequest);
+  }
+
+  update (listId: string, theList: IList) {
+    const urlRequest = `${this.serverUrl}/list/${listId}`;
+    return this.httpClient.put<IList>(urlRequest, theList);
+  }
+
+  delete (listId: string) {
+    const urlRequest = `${this.serverUrl}/list/${listId}`;
+    return this.httpClient.delete<IList>(urlRequest);
+  }
 }
