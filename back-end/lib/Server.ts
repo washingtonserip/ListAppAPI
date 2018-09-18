342b46a885518b428c7d7658aab11c78acaa6dbb
Washington Pires <4washingtonbr@gmail.com>
2018-09-18 19:06:18 -0300

    [back-end] Change server port.

--- back-end/lib/Server.ts
+++ back-end/lib/Server.ts
@@ -1,6 +1,6 @@
 import './DevelopmentEnviroment';
 import app from './App';
 
-const port = process.env.PORT || 3000;
+const port = 3000; // process.env.PORT ||
 
 app.listen(port, () => console.log('Express server listening on port ' + port));
